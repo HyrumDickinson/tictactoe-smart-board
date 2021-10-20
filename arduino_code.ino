7030d539165067dcbeb1ff029eb5f71887724baa
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-10-19 21:58:16 -0500

    setup Arduino code file - need to add more

---
+++ arduino_code.ino
@@ -0,0 +1,2 @@
+# Unfortunately, the Arduino IDE does not have a GitHub plugin - so the only way to get Arduino code to GitHub is via copy-paste. 
+# I will copy my Arduino code here once it is complete.
